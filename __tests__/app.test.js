6ef73d1af58e662067106abe97620271a8d55a47
HarCass <harry.jc.cass@gmail.com>
2023-03-27 11:19:48 +0100

    GET /api/topics basic functionality.

---
+++ __tests__/app.test.js
@@ -0,0 +1,26 @@
+const request = require('supertest');
+const app = require('../app');
+const db = require('../db/connection');
+const testData = require('../db/data/test-data/index');
+const seed = require('../db/seeds/seed');
+
+afterAll(() => db.end());
+beforeEach(() => seed(testData));
+
+describe('GET /api/topics', () => {
+    it('200: returns an array of all the topics.', () => {
+        return request(app)
+        .get('/api/topics')
+        .expect(200)
+        .then(({body}) => {
+            const {topics} = body;
+            expect(topics).toHaveLength(3);
+            topics.forEach(topic => {
+                expect(topic).toMatchObject({
+                    slug: expect.any(String),
+                    description: expect.any(String)
+                });
+            });
+        });
+    });
+});
