83df58cce44b62728cc56ca1f9bea9835dc2e022
HarCass <harry.jc.cass@gmail.com>
2023-03-28 11:35:02 +0100

    Changing how controller handles 404, so it doesnt check id exists while deleting it.

--- controllers/comments.js
+++ controllers/comments.js
@@ -2,7 +2,8 @@ const {removeCommentById, checkCommentIdExists} = require('../models/comments');
 
 exports.delCommentById = (req, res, next) => {
     const {comment_id} = req.params;
-    return Promise.all([removeCommentById(comment_id), checkCommentIdExists(comment_id)])
+    return checkCommentIdExists(comment_id)
+    .then(() => removeCommentById(comment_id))
     .then(() => res.status(204).send())
     .catch(next);
 }
