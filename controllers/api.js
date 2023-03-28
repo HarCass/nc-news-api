06a34633018868cd349d5e6b5d83750a39a90022
HarCass <harry.jc.cass@gmail.com>
2023-03-28 16:23:24 +0100

    Refactoring to remove circular dependancy node warning.

--- controllers/api.js
+++ controllers/api.js
@@ -1,4 +1,4 @@
 exports.getEndpoints = (req, res) => {
-    const endpoints = require('../endpoints.json');
-    res.status(200).send({endpoints});
+    const currentEndpoints = require('../endpoints.json');
+    res.status(200).send({endpoints: currentEndpoints});
 }
--- controllers/topics.js
+++ controllers/topics.js
@@ -1,4 +1,3 @@
-const { response } = require('../app');
 const {selectTopics} = require('../models/topics');
 
 exports.getTopics = (req, res, next) => {
