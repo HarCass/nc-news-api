684f63495955dc7b6a23b8faa363e4dc1fab4bb8
HarCass <harry.jc.cass@gmail.com>
2023-03-31 13:46:53 +0100

    GET /api/users/:username/comments

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,5 @@
-const { selectUsers, selectUserById } = require('../models/users');
+const { use } = require('../app');
+const { selectUsers, selectUserById, selectCommentsByUser, checkUsernameExists } = require('../models/users');
 
 exports.getUsers = (req, res, next) => {
     return selectUsers()
@@ -10,5 +11,12 @@ exports.getUserById = (req, res, next) => {
     const {username} = req.params;
     return selectUserById(username)
     .then(user => res.status(200).send({user}))
+    .catch(next);
+}
+
+exports.getCommentsByUser = (req, res, next) => {
+    const {username} = req.params;
+    return Promise.all([selectCommentsByUser(username), checkUsernameExists(username)])
+    .then(([comments]) => res.status(200).send({comments}))
     .catch(next);
 }
--- models/users.js
+++ models/users.js
@@ -12,3 +12,15 @@ exports.selectUserById = (username) => {
         return rows[0];
     });
 }
+
+exports.selectCommentsByUser = (username) => {
+    return db.query('SELECT * FROM comments WHERE author = $1', [username])
+    .then(({rows}) => rows);
+}
+
+exports.checkUsernameExists = (username) => {
+    return db.query('SELECT * FROM users WHERE username = $1', [username])
+    .then(({rows}) => {
+        if(!rows.length) return Promise.reject({status:404, msg: 'Username Not Found'});
+    });
+}
