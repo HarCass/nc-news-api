382a2aeb9e634dde6702f95f70b709056c340582
HarCass <harry.jc.cass@gmail.com>
2024-10-04 19:50:28 +0100

    fixing host

--- listen.ts
+++ listen.ts
@@ -2,7 +2,7 @@ import app from "./app.js";
 
 const PORT = process.env.PORT ? parseInt(process.env.PORT) : 9090;
 
-app.listen({port: PORT}, (err) => {
+app.listen({port: PORT, host: "0.0.0.0"}, (err) => {
     if (err) {
         console.log(err);
         process.exit(1);
