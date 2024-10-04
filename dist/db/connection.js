f93e00c462cd96bb5f21bbccb71210df1257e97c
HarCass <harry.jc.cass@gmail.com>
2024-10-04 18:19:36 +0100

    hanging connections again

--- dist/db/connection.js
+++ dist/db/connection.js
@@ -12,7 +12,7 @@ if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
 }
 const dbConfig = ENV === 'production' ? {
     connectionString: process.env.DATABASE_URL,
-    max: 5,
+    max: 3,
     ssl: { rejectUnauthorized: false }
 } : {
     user: process.env.DB_USER,
